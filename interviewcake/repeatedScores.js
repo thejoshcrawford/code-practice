26be0debdc289141a2fd2de2d4cd55e4160c49cc
Josh Crawford <josh@crawford.cc>
2019-05-24 19:12:52 -0700

    Added repeating scores

---
+++ interviewcake/repeatedScores.js
@@ -0,0 +1,76 @@
+function sortScores(unorderedScores, highestPossibleScore) {
+
+  // Sort the scores in O(n) time
+  let scores = {};
+  
+  for(let score of unorderedScores) {
+    scores[score] = (scores[score] || 0) +1;
+  }
+  // console.log(scores);
+  
+  let orderedScores = [];
+  for(let i = highestPossibleScore; i > 0; i--) {
+    if (scores[i]) {
+      for(let j = 0; j < scores[i]; j++) {
+        orderedScores.push(i);
+      }
+      // console.log(scores[i])
+    }
+  }
+  // console.log(orderedScores);
+
+
+  return orderedScores;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Tests
+
+let desc = 'no scores';
+let actual = sortScores([], 100);
+let expected = [];
+assertEqual(JSON.stringify(actual), JSON.stringify(expected), desc);
+
+desc = 'one score';
+actual = sortScores([55], 100);
+expected = [55];
+assertEqual(JSON.stringify(actual), JSON.stringify(expected), desc);
+
+desc = 'two scores';
+actual = sortScores([30, 60], 100);
+expected = [60, 30];
+assertEqual(JSON.stringify(actual), JSON.stringify(expected), desc);
+
+desc = 'many scores';
+actual = sortScores([37, 89, 41, 65, 91, 53], 100);
+expected = [91, 89, 65, 53, 41, 37];
+assertEqual(JSON.stringify(actual), JSON.stringify(expected), desc);
+
+desc = 'repeated scores';
+actual = sortScores([20, 10, 30, 30, 10, 20], 100);
+expected = [30, 30, 20, 20, 10, 10];
+assertEqual(JSON.stringify(actual), JSON.stringify(expected), desc);
+
+function assertEqual(a, b, desc) {
+  if (a === b) {
+    console.log(`${desc} ... PASS`);
+  } else {
+    console.log(`${desc} ... FAIL: ${a} != ${b}`);
+  }
+}
